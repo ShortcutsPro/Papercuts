c541f39856ce4250dff59241d5c96808315d014d
ShortcutsPro <ShortcutPros@users.noreply.github.com>
2021-08-01 08:18:56 -0500

    a

--- repo.js
+++ repo.js
@@ -42,7 +42,7 @@ async function load(repo, dryrun) {
 
         if (!dbpackage.depiction) {
           dbpackage.depiction =
-            "https://easrng.github.io/papercuts/fallback-depiction.html#" +
+            "https://shortcutspro.github.io/Papercuts/fallback-depiction.html#" +
             encodeURIComponent(JSON.stringify(dbpackage));
         }
         db.packages.push(dbpackage);
@@ -103,7 +103,7 @@ export async function init() {
   db.repos = [];
   db.packages = [];
   sources = JSON.parse(
-    localStorage.getItem("sources") || '["https://shortcutspro.github.io/library/"]'
+    localStorage.getItem("sources") || '["https://shortcutspro.github.io/library/meta.json"]'
   );
   await Promise.all(sources.map(e => load(e)));
 }
